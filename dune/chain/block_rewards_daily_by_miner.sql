1aa6d80ec5f7fa40dc74112ec9b6b3beddf4a5ea
BigQuery AutoFormatter <test@example.com>
2021-01-30 20:33:34 +0000

    Auto-format queries

--- dune/chain/block_rewards_daily_by_miner.sql
+++ dune/chain/block_rewards_daily_by_miner.sql
@@ -1,11 +1,13 @@
 #standardSQL
 -- MIT License
 -- Copyright (c) 2019 Yaz Khoury, yaz.khoury@gmail.com
-
-
-SELECT miner, 
-    DATE(timestamp) as date,
-    COUNT(miner) as total_block_reward
-FROM `bigquery-public-data.crypto_ethereum_classic.blocks` 
-GROUP BY miner, date
-HAVING COUNT(miner) > 1
+SELECT
+  miner,
+  DATE(timestamp) AS date,
+  COUNT(miner) AS total_block_reward
+FROM
+  `bigquery-public-data.crypto_ethereum_classic.blocks`
+GROUP BY
+  miner,
+  date
+HAVING COUNT(miner) > 1;
--- dune/chain/rich_list.sql
+++ dune/chain/rich_list.sql
@@ -1,34 +1,50 @@
 #standardSQL
 -- MIT License
 -- Copyright (c) 2018 Evgeny Medvedev, evge.medvedev@gmail.com
-with double_entry_book as (
+WITH
+  double_entry_book AS (
     -- debits
-    select to_address as address, value as value
-    from `bigquery-public-data.crypto_ethereum_classic.traces`
-    where to_address is not null
-    and status = 1
-    and (call_type not in ('delegatecall', 'callcode', 'staticcall') or call_type is null)
-    union all
-    -- credits
-    select from_address as address, -value as value
-    from `bigquery-public-data.crypto_ethereum_classic.traces`
-    where from_address is not null
-    and status = 1
-    and (call_type not in ('delegatecall', 'callcode', 'staticcall') or call_type is null)
-    union all
-    -- transaction fees debits
-    select miner as address, sum(cast(receipt_gas_used as numeric) * cast(gas_price as numeric)) as value
-    from `bigquery-public-data.crypto_ethereum_classic.transactions` as transactions
-    join `bigquery-public-data.crypto_ethereum_classic.blocks` as blocks on blocks.number = transactions.block_number
-    group by blocks.miner
-    union all
-    -- transaction fees credits
-    select from_address as address, -(cast(receipt_gas_used as numeric) * cast(gas_price as numeric)) as value
-    from `bigquery-public-data.crypto_ethereum_classic.transactions`
-)
-select address, 
-sum(value) / 1000000000 as balance
-from double_entry_book
-group by address
-order by balance desc
-limit 20 
+    SELECT
+      to_address AS address,
+      value AS value
+    FROM
+      `bigquery-public-data.crypto_ethereum_classic.traces`
+    WHERE
+      to_address IS NOT null AND status = 1 AND (call_type NOT IN ('delegatecall', 'callcode', 'staticcall') OR
+      call_type IS null)
+    UNION ALL -- credits
+    SELECT
+      from_address AS address,
+      - value AS value
+    FROM
+      `bigquery-public-data.crypto_ethereum_classic.traces`
+    WHERE
+      from_address IS NOT null AND status = 1 AND (call_type NOT IN ('delegatecall', 'callcode', 'staticcall') OR
+      call_type IS null)
+    UNION ALL -- transaction fees debits
+    SELECT
+      miner AS address,
+      sum(CAST(receipt_gas_used AS numeric) * CAST(gas_price AS numeric)) AS value
+    FROM
+      `bigquery-public-data.crypto_ethereum_classic.transactions` AS transactions
+      JOIN
+      `bigquery-public-data.crypto_ethereum_classic.blocks` AS blocks
+      ON blocks.number = transactions.block_number
+    GROUP BY
+      blocks.miner
+    UNION ALL -- transaction fees credits
+    SELECT
+      from_address AS address,
+      - (CAST(receipt_gas_used AS numeric) * CAST(gas_price AS numeric)) AS value
+    FROM
+      `bigquery-public-data.crypto_ethereum_classic.transactions`
+  )
+SELECT
+  address,
+  sum(value) / 1000000000 AS balance
+FROM
+  double_entry_book
+GROUP BY
+  address
+ORDER BY balance DESC
+LIMIT 20;
