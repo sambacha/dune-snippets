92bbeb57c52ecef66e447a5ac38c3ae9e914074e
BigQuery AutoFormatter <test@example.com>
2021-02-11 01:48:35 +0000

    Auto-format queries

--- dune/gas/MEV_ZeroCostTx.sql
+++ dune/gas/MEV_ZeroCostTx.sql
@@ -1,6 +1,10 @@
-select t.*, b.miner --t.block_time, t.success, t."hash", t."from", t."to", b.miner, t.index, t.gas_used, t.nonce, t.block_number, t.block_hash, t.gas_limit, t.gas_price, t.data
-from ethereum.transactions t
-join ethereum.blocks b
-    on t.block_hash = b.hash
-where t.gas_price = 0
-    and t.block_time >= '2021-01-01'
+SELECT
+  t.*,
+  b.miner --t.block_time, t.success, t."hash", t."from", t."to", b.miner, t.index, t.gas_used, t.nonce, t.block_number, t.block_hash, t.gas_limit, t.gas_price, t.data
+FROM
+  ethereum.transactions AS t
+  JOIN
+  ethereum.blocks AS b
+  ON t.block_hash = b.`hash`
+WHERE
+  t.gas_price = 0 AND t.block_time >= '2021-01-01';
