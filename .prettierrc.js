d2dee6ca2634f00bc52d7571e29728ac2f91e887
sam bacha <sam@freighttrust.com>
2021-03-13 03:35:46 -0800

    cfg(prettier): update

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,3 @@
-/**
- * @file Prettier configuration for Solidity
- * @version 1.0.3
- * @summary base config adapted from AirBNB to minizmie diff churn
- * @overrides solidity settings from Solidity Documentation
- */
-
 'use strict';
 
 module.exports = {
