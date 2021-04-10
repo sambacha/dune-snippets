c61546ac66957560662bc1eb280c4fdea27a330d
BigQuery AutoFormatter <test@example.com>
2021-04-10 07:13:34 +0000

    Auto-format queries

--- dune/homestead/daily_top_miners.sql
+++ dune/homestead/daily_top_miners.sql
@@ -1,4 +1,11 @@
-SELECT miner, DATE (timestamp) AS date,
-    COUNT (miner) AS total_block_reward FROM
-  `bigquery - public - data.crypto_ethereum_classic.blocks` GROUP BY miner,
-    date HAVING COUNT (miner) > 100 ORDER BY date, COUNT (miner);
+SELECT
+  miner,
+  DATE(timestamp) AS date,
+  COUNT(miner) AS total_block_reward
+FROM
+  `bigquery - public - data.crypto_ethereum_classic.blocks`
+GROUP BY
+  miner,
+  date
+HAVING COUNT(miner) > 100
+ORDER BY date, COUNT(miner);
